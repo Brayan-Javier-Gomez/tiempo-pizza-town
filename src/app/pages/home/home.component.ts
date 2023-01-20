cb1c70587455f23aad6d292388476d98d8b7de0b
Brayan Javier <brayanjaviergomez.g@gmail.com>
2023-01-19 22:03:36 -0500

    Home changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x!.className === "topnav") {
+      x!.className += " responsive";
+    } else {
+      x!.className = "topnav";
+    }
+  }
+}
