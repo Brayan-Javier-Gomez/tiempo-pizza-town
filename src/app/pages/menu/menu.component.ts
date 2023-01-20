68be41609e1d4e53b1d3d8755dde69d4076a1b44
Brayan Javier <brayanjaviergomez.g@gmail.com>
2023-01-20 10:09:52 -0500

    Menu Line decoration

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -13,4 +13,13 @@ SwiperCore.use([EffectCoverflow]);
 })
 export class MenuComponent {
 
+  myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x!.className === "topnav") {
+      x!.className += " responsive";
+    } else {
+      x!.className = "topnav";
+    }
+  }
+
 }
