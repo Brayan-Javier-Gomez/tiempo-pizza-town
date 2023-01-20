e0d73674fedb68007ce2f169fa8254fe3c02da01
Brayan Javier <brayanjaviergomez.g@gmail.com>
2023-01-19 19:52:25 -0500

    Responsive

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'menu',
+    loadChildren: () =>
+      import('./pages/menu/menu.module').then((m) => m.MenuModule),
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: 'enabled',
+    }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/pages/menu/menu.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MenuRoutingModule } from './menu-routing.module';
+import { MenuComponent } from './menu.component';
+import { SwiperModule } from 'swiper/angular';
+
+
+@NgModule({
+  declarations: [MenuComponent],
+  imports: [
+    CommonModule,
+    MenuRoutingModule,
+    SwiperModule
+  ]
+})
+export class MenuModule { }
